7af1aceaa54f95108673782836b3557e8d185296
Ravi Yadav <121853042+raviyadavh@users.noreply.github.com>
2025-03-07 08:57:34 +0530

    ridection issue fixed

--- src/common/components/createLeadDialog.js
+++ src/common/components/createLeadDialog.js
@@ -1,58 +1,76 @@
   import React, { useState } from 'react';
   import apiService from '../services/apiService';
+  import axios from 'axios';
   const CreateLeadDialog = ({ isOpen, onClose, userDetail }) => {
     const [loanType, setLoanType] = useState();
    
-    
+    let dynamicURL = window.location.origin.includes('staging') ? 'https://staging.newfi.com/' : 'https://newfi.com/';
+    const API_URL = dynamicURL || 'https://staging.newfi.com/';
     if (!isOpen) return null; // If the modal is not open, render nothing
   
     const createLead = () => {
+      let createdBy = sessionStorage.getItem("wp_logged_in_user");
+
+      if (!createdBy) {
+        const nameElement = document.querySelectorAll('.display-name')[0]; // Get the first element
+        createdBy = nameElement ? nameElement.textContent.trim() : ''; // Update the existing variable
+      }
+
       let reqObj = {
         firstName: userDetail.first_name,
         lastName: userDetail.last_name,
         phoneNumber: userDetail.phone_number,
-        email_id: userDetail.email,
+        email: userDetail.email_id,
         propertyState: userDetail.referralPropertyState,
         loanPurpose: loanType,
-        createdBy: sessionStorage.getItem("wp_logged_in_user")
+        createdBy: createdBy
       }
  
-      apiService.post('rest/crm/createDashboardLead', JSON.stringify(reqObj), { headers: { 'Content-Type': 'application/json', }, cache: 'no-cache' }).then((data) => {
-        if (data.resultObject !== null && data.resultObject !== "Failure") {
-          let envUrl = data.resultObject.url.replace(/(loancenter\/)/, '');
-          if ((data.resultObject.duplicateAccount != undefined && data.resultObject.duplicateLead != undefined) && (data.resultObject.duplicateAccount == true || data.resultObject.duplicateLead == true)) {
+      apiService.post('rest/crm/createDashboardLead', JSON.stringify(reqObj), { headers: { 'Content-Type': 'application/json', }, cache: 'no-cache' }).then((response) => {
+        if (response.data.resultObject !== null && response.data.resultObject !== "Failure") {
+          let envUrl = API_URL;
+          let crmId = response.data.resultObject;
+
+          if (crmId) {
+    //         $email = $request->get_param('email');
+    // $phone_number = $request->get_param('phoneNumber');
+    // $crm_id = $request->get_param('crmId');
+      axios.post(`${API_URL}wp-json/newfi/v1/updateLeadInfo?phoneNumber=${userDetail.phone_number}&email=${userDetail.email_id}&crmId=${crmId}`, {}).then(response => {
+          console.log(response);
+        }).catch(error => {
+          console.log(error);
+        });
             window.setTimeout(() => {
-              if (data.resultObject.duplicateAccount == true && (data.resultObject.duplicateLead != undefined && data.resultObject.duplicateLead == true)) {
-                window.location.href = data.resultObject.url;
-              } else if ((data.resultObject.duplicateLead != undefined && data.resultObject.duplicateLead == true) && data.resultObject.duplicateAccount == false) {
-                if (loanType == 'PUR') {
-                  window.location.href = envUrl + "preapproval/#/" + data.resultObject.crmId + "/";
-                } else if (loanType == 'REFNSAM') {
-                  window.location.href = envUrl + "equitychoice/#/" + data.resultObject.crmId + "/";
-                } else {
-                  window.location.href = envUrl + "savings/#/" + data.resultObject.crmId + "/";
-                }
-              }
+              if (loanType == 'PUR') {
+                    window.location.href = envUrl + "preapproval/#/" + response.data.resultObject + "/";
+                  } else if (loanType == 'REFNSAM') {
+                    window.location.href = envUrl + "equitychoice/#/" + response.data.resultObject + "/";
+                  } else {
+                    window.location.href = envUrl + "savings/#/" + response.data.resultObject + "/";
+                  }
+              // if (response.data.resultObject.duplicateAccount == true && (response.data.resultObject.duplicateLead != undefined && response.data.resultObject.duplicateLead == true)) {
+              //   window.location.href = response.data.resultObject.url;
+              // } else if ((response.data.resultObject.duplicateLead != undefined && response.data.resultObject.duplicateLead == true) && response.data.resultObject.duplicateAccount == false) {
+              //   if (loanType == 'PUR') {
+              //     window.location.href = envUrl + "preapproval/#/" + response.data.resultObject + "/";
+              //   } else if (loanType == 'REFNSAM') {
+              //     window.location.href = envUrl + "equitychoice/#/" + response.data.resultObject + "/";
+              //   } else {
+              //     window.location.href = envUrl + "savings/#/" + response.data.resultObject + "/";
+              //   }
+              // }
             }, 5000);
-          } else if (data.resultObject.otpStatus == 'FAILED' && data.resultObject.message == 'Duplicate Lead') {
+          } else if (response.data.error) {
             let redirectUrl;
-            if (loanType === "PUR") {
-              redirectUrl = `${envUrl}preapproval/#/${data.resultObject.crmId}/`;
-            } else if (loanType === "REFNSAM") {
-              redirectUrl = `${envUrl}equitychoice/#/${data.resultObject.crmId}/`;
-            } else {
-              redirectUrl = `${envUrl}savings/#/${data.resultObject.crmId}/`;
+            if(response.data.error.message.includes('duplicate')){
+              // 404
+              console.log('duplicate');
+             // redirectUrl = envUrl + "404";
             }
             window.location.href = redirectUrl;
           } else {
-            if (loanType == 'PUR') {
-              window.location.href = envUrl + "preapproval/#/" + data.resultObject.crmId + "/";
-            } else if (loanType == 'REFNSAM') {
-              window.location.href = envUrl + "equitychoice/#/" + data.resultObject.crmId + "/";
-            } else {
-              window.location.href = envUrl + "savings/#/" + data.resultObject.crmId + "/";
-            }
-  
+            //404
+            //redirectUrl = envUrl + "404";
           }
         }
       })
